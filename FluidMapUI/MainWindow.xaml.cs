be7051e3d426da50e57caab4ae1c521f2010fbae
Bishal Shrestha <bishal@bigtyre.com.au>
2024-10-29 12:33:30 +1000

    - Added Mapsui support - Added navigation to Australia

--- FluidMapUI/MainWindow.xaml.cs
+++ FluidMapUI/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Mapsui;
+using Mapsui.Extensions;
+using Mapsui.Projections;
+using Mapsui.Tiling;
+using Mapsui.UI.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +28,33 @@ namespace FluidMapUI
         public MainWindow()
         {
             InitializeComponent();
+
+            WindowState = WindowState.Maximized;
+
+            var mapControl = new MapControl();
+            var map = mapControl.Map;
+
+            //Adding layer of open street map 
+            map.Layers.Add(OpenStreetMap.CreateTileLayer());
+
+            MapControl.Map = map;
+
+            NavigateToAustralia();
+        }
+
+        private async void NavigateToAustralia()
+        {
+            await Task.Delay(500);
+
+            var target = new MPoint(133.7751, -25.2744); //Australia
+
+            //OSM uses spherical mercator coordinates. So transform the long lat coordinates to the spherical mercator
+            var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(target.X, target.Y).ToMPoint();
+
+            var map = MapControl.Map;
+            var navigator = map.Navigator;
+
+            navigator.CenterOnAndZoomTo(sphericalMercatorCoordinate, navigator.Resolutions[5], duration: 2000);
         }
     }
 }
